26f5cba9e2097fa4a2fb0fa488a1119289c90d77
AJ Bee <angelajobennett@yahoo.com>
2022-05-11 11:02:30 -0500

    third commit

---
+++ src/main/java/com/galvanize/AllEqual.java
@@ -0,0 +1,17 @@
+
+public class AllEqual {
+
+    public static void main(String[] args) {
+        int i = 45;
+
+        while (i < 51) {
+            System.out.println(i);
+            i = i + 1;
+        }
+
+        for (int j = 2; j < 7; j++) {
+            if (j%2 == 0){
+                System.out.println(j);}
+        }
+    }
+}
