c1a2527d01f29902dc8121996fd2f558fa103c0d
Angela Bennett <angela.bennett.dzeb@statefarm.com>
2022-09-12 10:57:31 -0500

    initial commit

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -5,9 +5,9 @@ public class Application {
 
     public static void main(String[] args) {
 
-        String firstArgument = args[0];   // arg1
-        String secondArgument = args[1];  // arg2
+       // String firstArgument = args[0];   // arg1
+       // String secondArgument = args[1];  // arg2
 
-        System.out.println("Hello worlds");
+       System.out.println("Hello worlds");
     }
 }
--- src/main/java/com/galvanize/SomeProgram.java
+++ src/main/java/com/galvanize/SomeProgram.java
@@ -4,8 +4,9 @@ public class SomeProgram {
     public static void main(String[] args) {
         if (args.length < 1){
             System.out.println("Please provide an argument");
+            System.exit(1);
         } else {
-            System.out.println("Argument provided is", args[0]);
+            System.out.println("Argument provided is " + args[0]);
         }
        // String firstArgument = args[0];   // arg1
       //  String secondArgument = args[1];  // arg2
